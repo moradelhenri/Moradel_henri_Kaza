6adb15135f1e89425fce626cb0c831031eec71ce
moradelhenri <83418498+moradelhenri@users.noreply.github.com>
2023-02-06 21:21:34 +0100

    preparation bd

--- kaza/src/components/header.jsx
+++ kaza/src/components/header.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/imgs/logo.png";
 export default function Header() {
 
   return (
-    <div className=" d_home">
+   
       <div  className ="box_header">
     <header id="header" className="flex Header">
       <div>
@@ -11,12 +11,12 @@ export default function Header() {
       </div>
 
       <div className="_nav">
-        <a href="#"  className="_navlinkAccueil">Accueil</a>
-        <a href="#" className ="_navlinkAPropos">A propos</a>
+        <a href="#"className="_navlinkAccueil">Accueil</a>
+        <a href="#"className ="_navlinkAPropos">A propos</a>
                </div>
     </header>    
   </div>
-  </div>
+
   
   );  
 }
