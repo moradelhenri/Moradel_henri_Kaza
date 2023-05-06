7969226d401160c7f380ad4d9ba990e2e597cbe6
moradelhenri <83418498+moradelhenri@users.noreply.github.com>
2023-05-06 20:27:15 +0200

    reparation build

--- src/pages/Fiche.js
+++ src/pages/Fiche.js
@@ -1,18 +1,113 @@
-//import React, { useState, useEffect } from "react";
-//import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 //import Cards from "../components/Cards";
-////import Header from "../components/Header";
-//import LocationData from "../data/fichier.json";
-//import fichier from "../data/fichier.json";
-
-//
-
-import React from 'react'
+import axios from "axios";
+import Header from "../components/Header";
+import ImageSlider from "../components/ImageSlider";
+import Tag from "../components/Tag";
+import Stars from "../components/Stars";
+import Accordion from "../components/Accordion";
+import Footer from "../components/Footer";
 
 function Fiche() {
+  const [data, setData] = useState({});
+  const { id } = useParams(); // fiche/id-de-la-fiche
+  const tags = data && data.tags;
+  const slides = data && data.pictures;
+ 
+  const Description = data && data.description 
+  const Equipments = data && data.equipments
+  
+ 
+  useEffect(() => {
+    axios
+      .get("../data/data.json")
+      .then((res) => {
+        const ficheData = res.data.find((x) => x.id === id);
+        console.log(ficheData);
+        setData(ficheData);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
   return (
-    <div>Fiche</div>
-  )
+    <div className="housing_sheet">
+      <Header />
+      {data.id ? (
+        <div className="name">
+          <ImageSlider slides={slides} />
+          <div className="name-container-fill">
+          <h1 className="name-container-title">{data.title}</h1>
+          <h3 className="name-container-location">{data.location}</h3>
+          <div className="boxTag">
+            {tags.map((tag) => (
+              <Tag key={tag} tag={tag} />
+            ))}
+          
+
+        
+          </div>
+           <div className="name_host"><img
+              className="host_owner_picture"
+              src={data?.host.picture}
+              alt="Propriétaire"
+            />
+
+            <span className="name__host">{data?.host.name}</span>
+             </div> 
+             
+          <div className="starBox">
+            <Stars elem={data.rating} />
+          </div>   
+         
+
+         
+  <div className="wapper_accordion">   
+
+<div className=" accordion">
+<div className="box-equipement">
+  
+<Accordion Title="Equipements" 
+
+Text= { Equipments }
+   /> 
+  </div>
+
+<div className="box-description">
+<Accordion Title="Description" 
+
+Text= { Description }
+/> 
+</div>
+
+ </div> 
+
+
+</div>
+
+
+
+
+
+
+
+
+
+ 
+</div>
+        
+        
+           
+       </div> 
+    
+        
+      ) : null}
+      ;
+      <div className="ok"></div>
+      <Footer/>
+    </div> 
+    
+  );
 }
 
-export default Fiche
+export default Fiche;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,53 +1,50 @@
+
 import Card from "../components/card";
 import Header from "../components/Header";
+
 import React, { useState, useEffect } from "react";
-import axios from "axios"
+import { useNavigate } from "react-router-dom";
+
+import axios from "axios";
+
 import "../styles.css";
+import Footer from "../components/Footer";
 
 function Home() {
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    axios
+      .get("../data/data.json")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
-const [data, setData] = useState([]);
+  function handleCardClick(id) {
+    navigate("fiche/" + id);
+  }
 
-useEffect(()=> {
-  axios.get('../data/data.json')
-  .then((res) => setData(res.data))
-     .catch(err=>console.log(err))
-      console.log(data);
-   
-  }, [])
-  
-  return (   
-   
+  return (
     <section>
-<Header/>
+      <Header />
+
       <div className="herobox">
         <h1 className="legend">Chez vous, partout et ailleurs</h1>
       </div>
-      <ul
-        className="box_section_galerie"
-      
-      >
-       
-        {
-      //  data && data.length>0 && data.map((item)=><p>{item.about}</p>)
-     }
-      </ul>{data.map((data, id) => (
-         <li key={data.id}> 
-         <Card cover={data.cover} title={data.title} /></li> 
-         
+   <div className="box_section_galerie">
+      {data.map((data, id) => (
+        <li key={data.id} onClick={() => handleCardClick(data.id)}>
+          <Card cover={data.cover} title={data.title} />
+        </li>
+      ))}
 
-        
-        ))}
-
-      <div>
    
-    
+
       </div>
+       <Footer/> 
     </section>
   );
 }
-
-
 
 export default Home;
