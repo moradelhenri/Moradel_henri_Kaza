4cf3256ed462fdc33a6c638b8a2b94597abe1f71
moradelhenri <83418498+moradelhenri@users.noreply.github.com>
2023-08-06 18:37:11 +0200

    integration responsive accueil a propos

--- kaza/src/components/Footer.js
+++ kaza/src/components/Footer.js
@@ -6,7 +6,7 @@ function  Footer() {
     return(
         <footer>
             <div id="container-footer">
-                <img className="img-footer" src={logoFooter} alt="Logo" />
+              <div className=" box-img-footer">  <img className="img-footer" src={logoFooter} alt="Logo" /></div>
                 <p className="text-footer">© 2020 Kasa. 2022 Kasa. Tous droits réservés</p>
             </div>
         </footer>
